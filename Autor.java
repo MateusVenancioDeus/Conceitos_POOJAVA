f29d656ac3de513c0adc14fecf9d0873e60ac5b9
MATEUS VITOR VENANCIO DE DEUS <162513571+MateusVenancioDeus@users.noreply.github.com>
2025-08-22 21:03:17 -0300

    Autor.java(22-09)

---
+++ Autor.java
@@ -0,0 +1,13 @@
+public class Autor {
+    String nome;
+    String email;
+    String cpf;
+
+    void mostrarDetalhes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Valor: " + cpf);
+        System.out.println("------");
+    }
+
+}
